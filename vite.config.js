fd745628da06203f029a667e8ce12eb5c591f7a8
unknown <jesumathu11@gmail.com>
2025-04-09 16:02:01 +0530

    last deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio-site/', // <- replace with your GitHub repo name
   plugins: [react()],
 })
